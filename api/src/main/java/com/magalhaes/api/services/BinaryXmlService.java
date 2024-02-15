ec4e69a9171d19c6fd4008c5d3b69ef397685255
Marcos Magalhães <marcosvmagalhaes123@gmail.com>
2024-02-15 19:09:15 -0300

    docker adicionado

--- api/src/main/java/com/magalhaes/api/services/BinaryXmlService.java
+++ api/src/main/java/com/magalhaes/api/services/BinaryXmlService.java
@@ -1,7 +1,5 @@
 package com.magalhaes.api.services;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -12,23 +10,16 @@ import com.magalhaes.api.repositories.BinaryXmlRepository;
 
 @Service
 public class BinaryXmlService {
-    private static final Logger logger = LoggerFactory.getLogger(InvoiceService.class);
 
     @Autowired
     private BinaryXmlRepository binaryXmlRepository;
 
-    public BinaryXml createBinaryXml(MultipartFile file, Invoice invoice){
+    public BinaryXml createBinaryXml(MultipartFile file, Invoice invoice) throws Exception{
         BinaryXml binaryXml = new BinaryXml();
-
-        try {
-            binaryXml.setInvoice(invoice);
-            binaryXml.setFileXml(file.getBytes());
-            binaryXmlRepository.save(binaryXml);
-            return binaryXml;
-        } catch (Exception e) {
-            logger.error("Error to convert XML: " + e.getMessage());
-            return null;
-        }
+        binaryXml.setInvoice(invoice);
+        binaryXml.setFileXml(file.getBytes());
+        binaryXmlRepository.save(binaryXml);
+         return binaryXml;
     }
     
 }
